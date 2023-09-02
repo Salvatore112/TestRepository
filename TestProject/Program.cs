fcbb599a3f8fa1f93d36ebbc9db378b6feb95e16
Pavel Averin <pasha2281337alfa@gmail.com>
2023-09-02 18:14:45 +0300

    after rebase commit

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -6,6 +6,8 @@ Console.WriteLine("The email has been changed!");
 
 Console.WriteLine("New branch commit");
 
+Console.WriteLine("After rebase");
+
 var testObject = new TestClass();
 
 Console.WriteLine(testObject.MyFunction(140));
