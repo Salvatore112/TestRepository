5ef0f365d5955bdbeb1ad5ec96e3d465ff0a1eb8
Pavel Averin <pasha2281337alfa@gmail.com>
2023-09-02 16:18:06 +0300

    New branch commit

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -4,6 +4,8 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("The email has been changed!");
 
+Console.WriteLine("New branch commit");
+
 var testObject = new TestClass();
 
 Console.WriteLine(testObject.MyFunction(140));
